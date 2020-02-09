0926b56a9492e05321e6f97421594b172a26f515
unknown <sureshkumar.ece15@gmail.com>
2020-02-09 05:48:09 +0530

    changes3

--- src/app/home/carousel/carousel.component.ts
+++ src/app/home/carousel/carousel.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit, Renderer, OnDestroy } from '@angular/core';
+import { BlogService } from 'src/app/services/blog.service';
 
 
 @Component({
     selector: 'app-carousel',
     templateUrl: './carousel.component.html',
-    styleUrls: ['./carousel.component.css']
+    styleUrls: ['./carousel.component.css'],
+    providers: [ BlogService ]
   })
   export class CarouselComponent implements OnInit {
+    allBlogs = [''];
+
+    constructor(private blogData: BlogService) {}
     ngOnInit() {
-    }
+      this.blogData.getAllBlogs().subscribe(
+        data => {
+         this.allBlogs = data;
+        },
+        error => {
+          console.log('Some is went wrong');
+        }
+      );
+      console.log(this.allBlogs);
+     }
 
   }
